2ca8633503584c0bc7cbdf8a2492085426d26be7
Quan <Quan@DESKTOP-6P1SAFI>
2023-11-02 20:59:22 +0700

    service3

--- Vietapp.Android/AccessibilityService.cs
+++ Vietapp.Android/AccessibilityService.cs
@@ -1,11 +1,7 @@
 ﻿using Android.AccessibilityServices;
 using Android.App;
-using Android.Content;
-using Android.OS;
 using Android.Service.Autofill;
 using Android.Views.Accessibility;
-using System;
-using System.Windows;
 
 namespace Vietapp.Droid
 {
@@ -13,29 +9,14 @@ namespace Vietapp.Droid
     [IntentFilter(new[] { "android.accessibilityservice.AccessibilityService" })]
     public class ForegroundAppTrackerService : AccessibilityService
     {
-
-        public event EventHandler StateChanged;
         public override void OnAccessibilityEvent(AccessibilityEvent e)
         {
-            if (e.EventType == EventType.WindowStateChanged)
-            {
-                if (e.PackageName != null)
-                {
-                    string foregroundApp = e.PackageName.ToString();
-                    ProcessForegroundApp(foregroundApp);
-                }
-            }
+            
         }
 
         public override void OnInterrupt()
         {
             // Handle interruptions or errors
-        }
-
-        public override void OnServiceConnected()
-        {
-            base.OnServiceConnected();
-            // Additional setup or initialization can be performed here
         }
 
         private void ProcessForegroundApp(string packageName)
@@ -44,5 +25,4 @@ namespace Vietapp.Droid
             // For instance, log or handle the foreground app change
         }
     }
-
 }
